beb64048ae7c86295f2b3af92702e2a15f230c14
YKArafa <yahyamohamedarafa@gmail.com>
2026-05-09 18:53:34 +0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import StudySessions from './components/StudySessions/StudySessions'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <StudySessions />
+    </div>
+  )
+}
+
+export default App
